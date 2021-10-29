b0fb1e33b041979b5b9eb7150f26f14537ac5441
Sean-Sander <51496344+Sean-Sander@users.noreply.github.com>
2021-10-28 23:50:29 -0400

    initial commit

---
+++ TestClasses.py
@@ -0,0 +1,50 @@
+import unittest
+from Classes import *
+
+class ClassesModuleTest(unittest.TestCase):
+    def setUp(self):
+        pass
+    
+    def test_pokemon_get_name(self):
+        p = Pokemon("Pogachu", Type.ELECTRIC, Type.NONE, 50)
+        self.assertEqual("Pogachu", p.get_name())
+
+    def test_pokemon_get_primary_type(self):
+        p = Pokemon("Pogachu", Type.ELECTRIC, Type.NONE, 50)
+        self.assertEqual(Type.ELECTRIC, p.get_primary_type())
+    
+    def test_pokemon_get_secondary_type(self):
+        p = Pokemon("Pogachu", Type.ELECTRIC, Type.NONE, 50)
+        self.assertEqual(Type.NONE, p.get_secondary_type())
+    
+    def test_pokemon_get_hp(self):
+        p = Pokemon("Pogachu", Type.ELECTRIC, Type.NONE, 50)
+        self.assertEqual(50, p.get_hp())
+    
+    def test_pokemon_set_name(self):
+        p = Pokemon("Pogachu", Type.ELECTRIC, Type.NONE, 50)
+        p.set_name("Pikachu")
+        self.assertEqual("Pikachu", p.get_name())
+    
+    def test_pokemon_set_primary_type(self):
+        p = Pokemon("Pogachu", Type.ELECTRIC, Type.NONE, 50)
+        p.set_primary_type(Type.WATER)
+        self.assertEqual(Type.WATER, p.get_primary_type())
+    
+    def test_pokemon_set_secondary_type(self):
+        p = Pokemon("Pogachu", Type.ELECTRIC, Type.NONE, 50)
+        p.set_secondary_type(Type.FIGHTING)
+        self.assertEqual(Type.FIGHTING, p.get_secondary_type())
+    
+    def test_pokemon_set_hp(self):
+        p = Pokemon("Pogachu", Type.ELECTRIC, Type.NONE, 50)
+        p.set_hp(10)
+        self.assertEqual(10, p.get_hp())
+    
+    def test_pokemon_attack(self):
+        p = Pokemon("Pogachu", Type.ELECTRIC, Type.NONE, 50)
+        self.assertIn(p.attack(), range(10))
+    
+    # Huh
+    def test_pokemon_take_damage(self):
+        pass
